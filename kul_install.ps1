b0907c58c3ced9effdb05797144e9e6fc0a39afb
KiesbauerCAD <162697748+KiesbauerCAD@users.noreply.github.com>
2024-09-10 06:09:55 +0000

    Remove TeamViewer

--- kul_install.ps1
+++ kul_install.ps1
@@ -2,7 +2,7 @@ if (!(Get-Command choco -ErrorAction SilentlyContinue)) {
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
-$packages = @("googlechrome", "7zip", "adobereader", "pdf24", "teamviewer")
+$packages = @("googlechrome", "7zip", "adobereader", "pdf24")
 foreach ($package in $packages) {
     choco install $package -y
 }
