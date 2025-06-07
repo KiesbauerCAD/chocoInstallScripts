1d79ed3f1841d981aa44adfa7251cc488953d6be
KiesbauerCAD <162697748+KiesbauerCAD@users.noreply.github.com>
2025-06-07 11:41:58 +0200

    Update wb_install.ps1

--- wb_install.ps1
+++ wb_install.ps1
@@ -6,4 +6,4 @@ $packages = @("firefox", "7zip", "irfanview", "greenshot", "pdf24", "adobereader
 foreach ($package in $packages) {
     choco install $package -y
 }
-choco install microsoft-office-deployment --params="'/64bit /Language:de-de /Product:Standard2024Volume /LicenseKey:$licenseKey'" -y
+choco install microsoft-office-deployment --params="'/64bit /Language:de-de /Product:Standard2024Volume /LicenseKey:$LicenseKey'" -y
