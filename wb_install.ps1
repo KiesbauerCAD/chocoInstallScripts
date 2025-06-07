2149540193a3b37205ab890059f88000fe50384e
KiesbauerCAD <162697748+KiesbauerCAD@users.noreply.github.com>
2025-06-07 08:32:16 +0000

    add wb_install

---
+++ wb_install.ps1
@@ -0,0 +1,13 @@
+param(
+    [string]$LicenseKey = ""
+)
+
+if (!(Get-Command choco -ErrorAction SilentlyContinue)) {
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+$packages = @("firefox", "7zip", "irfanview", "greenshot", "pdf24")
+foreach ($package in $packages) {
+    choco install $package -y
+}
+choco install microsoft-office-deployment --params="'/64bit /Language:de-de /Product:Standard2024Volume /LicenseKey:$LicenseKey'" -y
