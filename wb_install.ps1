939d9d37cd7a069dac8a9a83ee48dfbb3ff13faf
KiesbauerCAD <162697748+KiesbauerCAD@users.noreply.github.com>
2025-06-07 11:11:09 +0200

    Update wb_install.ps1
    Add adobereader

--- wb_install.ps1
+++ wb_install.ps1
@@ -6,7 +6,7 @@ if (!(Get-Command choco -ErrorAction SilentlyContinue)) {
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
-$packages = @("firefox", "7zip", "irfanview", "greenshot", "pdf24")
+$packages = @("firefox", "7zip", "irfanview", "greenshot", "pdf24", "adobereader")
 foreach ($package in $packages) {
     choco install $package -y
 }
