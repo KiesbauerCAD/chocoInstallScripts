aaa0ea67cbd583923e76ab1c5aafb2645e5e22e2
KiesbauerCAD <162697748+KiesbauerCAD@users.noreply.github.com>
2025-06-10 08:52:07 +0200

    Create choco_install.ps1

---
+++ choco_install.ps1
@@ -0,0 +1,3 @@
+if (!(Get-Command choco -ErrorAction SilentlyContinue)) {
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
