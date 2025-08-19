5ca5dd0504f72c57420533766a98aa944559e6f9
KiesbauerCAD <162697748+KiesbauerCAD@users.noreply.github.com>
2025-08-19 11:17:29 +0200

    Add files via upload

--- unifi_reinstall.ps1
+++ unifi_reinstall.ps1
@@ -1,3 +1,4 @@
+Set-Location "$env:LOCALAPPDATA\Microsoft\WindowsApps"
 winget.exe uninstall Ubiquiti.IdentityDesktop.Endpoint 
 taskkill.exe /IM Identity.exe /F 
 winget.exe install Ubiquiti.IdentityDesktop.Endpoint --version 1.1.9.131
