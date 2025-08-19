ffa31094cac9720740a54642a9c2ca3a502712da
KiesbauerCAD <162697748+KiesbauerCAD@users.noreply.github.com>
2025-08-19 11:45:04 +0200

    Add files via upload

--- unifi_reinstall.ps1
+++ unifi_reinstall.ps1
@@ -1,4 +1,4 @@
-winget.exe source update & 
-winget.exe uninstall Ubiquiti.IdentityDesktop.Endpoint --silent --accept-source-agreements & 
-taskkill.exe /IM Identity.exe /F & 
+winget.exe source update
+winget.exe uninstall Ubiquiti.IdentityDesktop.Endpoint --silent --accept-source-agreements
+taskkill.exe /IM Identity.exe /F
 winget.exe install Ubiquiti.IdentityDesktop.Endpoint --version 1.1.9.131 --silent --accept-source-agreements --accept-package-agreements
